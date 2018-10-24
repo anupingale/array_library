9762c187f8002e69cadbfa58eaea0ffdf012f461
Annapurna <44020027+anupingale@users.noreply.github.com>
2018-10-24 14:56:35 +0530

    Add myLibrary framework.

---
+++ bin/myLibrary.js
@@ -0,0 +1,26 @@
+const justify = function(limit,msg) {
+  let numberOfSpaces = limit-msg.length;
+  let spaces = new Array(numberOfSpaces).fill(" ").join("");
+  return msg+spaces;
+}
+
+let testNumber = {number:1};
+
+const incrementTestNumber = function() {
+  testNumber.number++;
+}
+
+const getSerialNumber = function() {
+  return testNumber.number;
+}
+
+const testLogs = function(args,actualResult,expectedResult,msg) {
+  let log = justify(5,getSerialNumber().toString()) + "|" + justify(30,msg) + "|";
+  log = log + justify(20,args.toString()) + "|";
+  log = log + justify(20,actualResult.toString()) + "|";
+  log = log + justify(20,expectedResult.toString()) + "|";
+  incrementTestNumber();
+  console.log(log);
+}
+
+exports.testLogs = testLogs;
